19fb451a74fce61c083e053c08743c9785aee7a0
Adlan Elias <adlanelias@MacBook-Pro-Adlan.local>
2019-06-11 00:23:04 +1000

    changes

--- react-slack-clone/src/components/SidePanel/UserPanel.js
+++ react-slack-clone/src/components/SidePanel/UserPanel.js
@@ -43,8 +43,8 @@ class UserPanel extends React.Component {
           <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
             {/* App Header */}
             <Header inverted floated="left" as="h2">
-              <Icon name="code" />
-              <Header.Content>DevChat</Header.Content>
+              <Icon name="chess king" />
+              <Header.Content>PogChat</Header.Content>
             </Header>
 
             {/* User Dropdown  */}
