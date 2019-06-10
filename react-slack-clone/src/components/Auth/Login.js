dac2b61a836efa1c548e5fe318625ad39a7d8518
Adlan Elias <adlanelias@MacBook-Pro-Adlan.local>
2019-06-10 23:37:54 +1000

    register users and firebase

--- react-slack-clone/src/components/Auth/Login.js
+++ react-slack-clone/src/components/Auth/Login.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from "react";
 
 class Login extends React.Component {
-  render () {
-    return (
-      <div>Login</div>
-    )
+  render() {
+    return <div>Login</div>;
   }
 }
 
