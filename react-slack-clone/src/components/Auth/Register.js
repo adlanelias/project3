f49761761350171af2359df1b18d894ab27a752f
Adlan Elias <adlanelias@MacBook-Pro-Adlan.local>
2019-06-11 03:06:45 +1000

    some changes to register page and upload the avatars to firebase

--- react-slack-clone/src/components/Auth/Register.js
+++ react-slack-clone/src/components/Auth/Register.js
@@ -131,7 +131,7 @@ class Register extends React.Component {
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="blue" textAlign="center">
+          <Header as="h1" icon color="blue" textAlign="center">
             <Icon name="chess king" color="blue" />
             Register for PogChat
           </Header>
